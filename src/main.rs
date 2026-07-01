32b7d09e7e597d98b2645627bcf41481df4324b3
Hakan <86249094+HDemir23@users.noreply.github.com>
2026-07-01 16:31:02 +0300

    main clearde functions seperated

--- src/main.rs
+++ src/main.rs
@@ -11,65 +11,18 @@ fn main() {
 
     let mut todos: Vec<Todo> = Vec::new();
     let mut next_id: u32 = 1;
-
+    
     loop {
         let command = read_input("command: add / list / done / delete / q");
         match command.as_str() {
 
-            "add" => {
-                let title = read_input("title please");
-                let task = read_input("please task");
-
-                if title == "q"{
-                    break;
-                }
-
-
-                todos.push(Todo { id: next_id, title, task, status: false });
-
-                next_id += 1;
-
-                println!("Todo added");
-            }
-
-            "list" => {
-                for todo in &todos {
-                    println!("{} - {} - {} - Done: {}", todo.id , todo.title, todo.task, todo.status);
-                }
-            }
-
-
-            "done" => {
-                let title = read_input("which title is done");
-
-                for todo in &mut todos {
-                    if todo.title == title {
-                        todo.status = true;
-                        println!("task completed")
-                    }
-                }
-            }
-
-            "delete" => {
-                let title = read_input("which task do u want to delete");
-
-                let can_del = todos
-                    .iter()
-                    .any(|todo| todo.title == title && todo.status ==true);
-
-
-                if can_del {
-                    todos.retain(|todo| todo.title != title);
-                    println!("deleted")
-                } else {
-                    println!("u can only delete finisged tasks");
-                }
-            }
-
+            "add" => add_todo( &mut next_id,  &mut todos),
+            "list" => list_todo(&todos),
+            "done" => finished_todo(&mut todos),
+            "delete" => delete_todo(&mut todos),
             "q" => {
                 break;
             }
-
             _ => {
                 println!("unknown command");
             }
@@ -78,7 +31,6 @@ fn main() {
     }
 
 }
-
 fn read_input(message: &str) -> String {
     println!("{}", message);
 
@@ -90,4 +42,46 @@ fn read_input(message: &str) -> String {
 
 
     input.trim().to_string()
+}
+fn add_todo ( id: &mut u32, todos: &mut Vec<Todo>) {
+
+    
+                let title = read_input("title please");
+                let task = read_input("please task");
+
+                if title == "q"{
+                    return;
+                }
+                todos.push(Todo { id: *id, title, task, status: false });
+
+                *id += 1;
+
+                println!("Todo added");
+    }
+fn list_todo (todos: &Vec<Todo>) {
+    for todo in todos {
+        println!("{} - {} - {} - {}", todo.id ,todo.title, todo.task, todo. status);
+    }
+}
+fn finished_todo (todos: &mut Vec<Todo>) {
+    let title = read_input("wich task finished");
+
+    for todo in todos.iter_mut() {
+        if todo.title == title {
+            todo.status = true;
+            println!("task completed");
+        }
+    }
+}
+fn delete_todo (todos: &mut Vec<Todo>) {
+    let title = read_input("title for deletion");
+
+    let can_del:bool = todos.iter().any(|todo: &Todo| todo.title == title && todo.status);
+
+    if can_del {
+        todos.retain(|todo| todo.title != title);
+        println!("deleted")
+    } else {
+        println!("u can only delete finisged tasks");
+    }
 }
